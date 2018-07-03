4802005ee1a7beab150bc5269892a90c80330ea6
Nonameentered <matthewshu88@gmail.com>
2018-07-03 00:32:45 -0400

    fixed links, formatting on pages

--- practice/footer.inc.php
+++ practice/footer.inc.php
@@ -1,4 +1,1 @@
-</div>
-</body>
-</html>
 
--- practice/header.inc.php
+++ practice/header.inc.php
@@ -5,20 +5,16 @@
 <link rel="stylesheet" href="../style/ItsAc.css">
 </head>
 
-<body>
-<div style="position:absolute; top:0; left:0;"><img src="../images/border.jpg"></div>
-<div style="position:absolute; top:0; left:10;"><img src="../images/ItsAc.jpg"></div>
-<div style="position:absolute; top:0; left:285;"><img src="../images/border.jpg"></div>
-
-<img src="../images/title.jpg" style="position:absolute; top:15; left:294;">
-
-<div style="position:absolute; top:240; left:185;">
-<a href="/index.html"><img src="../images/home.jpg" border=0></a><br>
-<a href="/calendar.html"><img src="../images/calendar.jpg" border=0></a><br>
-<a href="/season.html"><img src="../images/season.jpg" border=0></a><br>
-<a href="/links.html"><img src="../images/webgems.jpg" border=0></a><br>
-<a href="/team.html"><img src="../images/team.jpg" border=0></a><br>
-</div>
-
-<div style="position:absolute; top:98; left:305; width: 410; height:300;">
+    <body>
+        <header>
+            <a class="blair-header" href="index.html">Montgomery Blair Academic Team</a>
+        </header>
+        
+        <nav id="topnav">
+            <a class="bar-item active" href="index.html">Home</a>
+            <a class="bar-item" href="calendar.html">Calendar</a>
+            <a class="bar-item" href="season.html">Season</a>
+            <a class="bar-item" href="team.html">Team</a>
+            <a class="bar-item" href="links.html">Resources</a>
+        </nav>
 <p><font size="3"><b>Practice</b></font></p>
